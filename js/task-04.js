9d447f637aa1a4749aee449016dd8b6800fe6814
Logvina <irinlogvina9@gmail.com>
2021-05-20 12:42:52 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -4,3 +4,20 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
+const decrementBtnRef = document.querySelector('[data-action=decrement]');
+const incrementBtnRef = document.querySelector('[data-action=increment]');
+const valueRef = document.querySelector('#value');
+
+let counterValue = 0;
+
+function decrement () {
+    valueRef.textContent = counterValue -= 1;
+}
+
+function increment () {
+    valueRef.textContent = counterValue += 1;
+}
+
+decrementBtnRef.addEventListener('click', decrement);
+incrementBtnRef.addEventListener('click', increment);
+
